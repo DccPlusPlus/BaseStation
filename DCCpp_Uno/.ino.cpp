a233ebea7954213879b965afa7ac82a19804cd71
Mike Dunston <m_dunston@comcast.net>
2016-08-19 10:10:23 -0700

    adding eclipseArduino project file.

---
+++ DCCpp_Uno/.ino.cpp
@@ -0,0 +1,26 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2016-08-19 10:04:56
+
+#include "Arduino.h"
+#include "DCCpp_Uno.h"
+#include "PacketRegister.h"
+#include "CurrentMonitor.h"
+#include "Sensor.h"
+#include "SerialCommand.h"
+#include "Accessories.h"
+#include "EEStore.h"
+#include "Config.h"
+#include "Comm.h"
+void loop();
+void setup();
+ISR(TIMER1_COMPB_vect);
+ISR(TIMER3_COMPB_vect);
+void showConfiguration();
+
+
+#include "DCCpp_Uno.ino"
+
+#endif
