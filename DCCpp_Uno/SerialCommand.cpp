935534ed7e24ab0d56fea8dfa72f1bc056d5fd07
TwinDad <mark@chfrrailroad.net>
2017-02-01 16:10:39 -0500

    Remove WiThrottle from this branch, which should be LCDThrottle-specific.

--- DCCpp_Uno/SerialCommand.cpp
+++ DCCpp_Uno/SerialCommand.cpp
@@ -22,9 +22,6 @@ Part of DCC++ BASE STATION for the Arduino
 #include "Outputs.h"
 #include "EEStore.h"
 #include "Comm.h"
-#ifdef WITHROTTLE_SUPPORT
-#include "WiThrottle.hpp"
-#endif
 
 extern int __heap_start, *__brkval;
 
@@ -53,11 +50,6 @@ void SerialCommand::process(){
 
     while(INTERFACE.available()>0){    // while there is data on the serial line
      c=INTERFACE.read();
-#ifdef WITHROTTLE_SUPPORT
-     if (WiThrottle::isWTCommand(c)) {
-       WiThrottle::readCommand(c);
-     } else
-#endif
      if(c=='<')                    // start of new command
        sprintf(commandString,"");
      else if(c=='>')               // end of new command
@@ -71,16 +63,8 @@ void SerialCommand::process(){
     EthernetClient client=INTERFACE.available();
 
     if(client){
-#ifdef WITHROTTLE_SUPPORT
-      WiThrottle::sendIntroMessage();
-#endif
       while(client.connected() && client.available()){        // while there is data on the network
       c=client.read();
-#ifdef WITHROTTLE_SUPPORT
-      if (WiThrottle::isWTCommand(c)) {
-	WiThrottle::readCommand(c);
-      } else
-#endif
       if(c=='<')                    // start of new command
         sprintf(commandString,"");
       else if(c=='>')               // end of new command
